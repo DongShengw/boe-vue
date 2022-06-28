7b37ccee4997e59a0bd29b4b330cd2263f88b2a8
llyums <194773116@qq.com>
2022-06-28 14:43:13 +0800

    设备

--- src/components/admin/deviceList.vue
+++ src/components/admin/deviceList.vue
@@ -1,6 +1,5 @@
 <template>
   <div v-wechat-title="$route.meta.title" class="home" style="padding: 20px">
-    <!--    功能-->
 
     <!--    搜索-->
     <div style="margin: 10px 0">
@@ -15,13 +14,24 @@
         placeholder="所属机构"
         style="width: 20%; margin-right: 20px"
         clearable
-      ></el-select>
+      >
+      <el-option
+          v-for="item in mechanism"
+          :key="item.value"
+          :value="item.value"
+        />
+      </el-select>
       <el-select
         v-model="searchGroup"
         placeholder="所属分组"
         style="width: 20%; margin-right: 20px"
         clearable
-      ></el-select>
+      >
+      <el-option
+          v-for="item in group"
+          :key="item.value"
+          :value="item.value"
+        /></el-select>
       <el-input
         v-model="searchMAC"
         placeholder="MAC地址"
@@ -35,13 +45,23 @@
         placeholder="分辨率"
         style="width: 20%; margin-right: 20px"
         clearable
-      ></el-select>
+      ><el-option
+          v-for="item in resolving"
+          :key="item.value"
+          :value="item.value"
+        />
+      </el-select>
       <el-select
         v-model="searchState"
         placeholder="设备状态"
         style="width: 20%; margin-right: 20px"
         clearable
-      ></el-select>
+      ><el-option
+          v-for="item in state"
+          :key="item.value"
+          :label="item.label"
+          :value="item.value"
+        /></el-select>
       <el-select
         v-model="searchSystem"
         placeholder="系统版本"
@@ -51,7 +71,6 @@
         <el-option
           v-for="item in system"
           :key="item.value"
-          :label="item.label"
           :value="item.value"
         />
       </el-select>
@@ -60,7 +79,11 @@
         placeholder="当前计划"
         style="width: 20%; margin-right: 20px"
         clearable
-      ></el-select>
+        ><el-option
+          v-for="item in schedule"
+          :key="item.value"
+          :value="item.value"
+      /></el-select>
       <el-button style="margin-left: 10px; margin-right: 10px" @click="reset"
         >重置</el-button
       >
@@ -83,16 +106,22 @@
       <el-table-column prop="deviceGroup" label="所属分组" />
       <el-table-column prop="macAddress" label="MAC地址" />
       <el-table-column prop="resolvingPower" label="分辨率" />
-      <el-table-column prop="deviceState" label="设备状态" />
+      <el-table-column prop="deviceState" label="设备状态">
+        <template #default="scope">
+          <el-tag v-if="scope.row.deviceState==1" type="success">空闲</el-tag>
+          <el-tag v-if="scope.row.deviceState==2">播放</el-tag>
+          <el-tag v-if="scope.row.deviceState==0" type="danger">离线</el-tag>
+        </template>
+      </el-table-column>
       <el-table-column prop="deviceSystem" label="系统升级" />
       <el-table-column prop="deviceSchedule" label="当前计划" />
 
+    <!--    功能-->
       <el-table-column label="操作">
         <template #default="scope">
           <el-button @click="details">详情</el-button>
           <el-button @click="handleEdit(scope.row)">控制</el-button>
-          <el-button @click="handleEdit(scope.row)">刷新</el-button>
-          <el-button @click="edit">编辑</el-button>
+          <el-button @click="handleEdit(scope.row)">编辑</el-button>
           <el-popconfirm
             title="确认删除?"
             @confirm="handleDelete(scope.row.deviceId)"
@@ -123,10 +152,10 @@
       <el-dialog v-model="dv1" title="编辑设备" width="30%">
         <el-form :model="form" label-width="120px">
           <el-form-item label="设备名称">
-            <el-input v-model="form.sex" style="width: 80%" />
+            <el-input v-model="form.deviceName" style="width: 80%" />
           </el-form-item>
           <el-form-item label="所属分组">
-            <el-input v-model="form.email" style="width: 80%" />
+            <el-input v-model="form.deviceGroup" style="width: 80%" />
           </el-form-item>
         </el-form>
         <template #footer>
@@ -137,10 +166,19 @@
         </template>
       </el-dialog>
 
-      <el-dialog v-model="dv2" title="编辑设备" width="30%">
-        <el-form :model="form" label-width="120px">
+      <el-dialog v-model="dv2" title="设备详情" width="30%">
+        <el-form :model="form1" label-width="120px">
           <el-tabs type="border-card">
-            <el-tab-pane label="设备信息">User</el-tab-pane>
+            <el-tab-pane label="设备信息">设备型号：HiDPTAndroid Hi3751V553
+系统版本：BOE_iGallery32_V13103_V5.2.0
+设备IP:192.168.53.212
+信发版本：1.3.1
+有线MAC地址：A0BB3ED3861D
+无线MAC地址：A0BB3ED2F376
+运行内存：0.96 GB
+SN：T232BS200721000123
+存储空间： 4.65 GB可用（共 5.39 GB）
+激活时间：2022-06-23 10:40:12</el-tab-pane>
             <el-tab-pane label="安装信息">Config</el-tab-pane>
             <el-tab-pane label="状态信息">Role</el-tab-pane>
           </el-tabs>
@@ -148,7 +186,7 @@
         <template #footer>
           <span class="dialog-footer">
             <el-button @click="dv2 = false">取消</el-button>
-            <el-button type="primary" @click="save">确认</el-button>
+            <el-button type="primary" @click="save1">确认</el-button>
           </span>
         </template>
       </el-dialog>
@@ -167,35 +205,69 @@ export default {
   components: {},
   data() {
     return {
-      edit: 0,
-      searchName:'',
-      searchMechanism:'',
-      searchGroup:'',
-      searchMAC:'',
-      searchResolving:'',
-      searchState:'',
-      searchSchedule:'',
-      searchSystem:'',
+      searchName: "",
+      searchMechanism: "",
+      searchGroup: "",
+      searchMAC: "",
+      searchResolving: "",
+      searchState: "",
+      searchSchedule: "",
+      searchSystem: "",
       currentPage4: 1,
       pageSize4: 10,
       total: 0,
       dv1: false,
       dv2: false,
       form: {},
+      form1: {},
       tableData: [],
-      schedule:[],
+      schedule: [],
+      group: [],
+      state: [
+        {
+          label: "播放",
+          value: "2",
+        },
+        {
+          label: "空闲",
+          value: "1",
+        },
+        {
+          label: "离线",
+          value: "0",
+        },
+      ],
+      resolving: [
+        {
+          value: "1920*1080(横)",
+        },
+        {
+          value: "1080*1920(竖)",
+        },
+        {
+          value: "3840*2160(横)",
+        },
+        {
+          value: "2160*3840(竖)",
+        },
+      ],
+      mechanism: [
+        {
+          value: "浙江大学",
+        },
+        {
+          value: "浙大城市学院",
+        },
+      ],
       system: [
         {
           value: "已是最新",
-          label: "已是最新",
         },
         {
           value: "升级",
-          label: "升级",
         },
         {
           value: "有新版本",
-          label: "有新版本",
         },
       ],
     };
@@ -205,19 +277,19 @@ export default {
   },
   methods: {
     load() {
-      request
+      request 
         .get("/device", {
           params: {
             pageNum: this.currentPage4,
             pageSize: this.pageSize4,
             name: this.searchName,
             mechanism: this.searchMechanism,
-            group:this.searchGroup,
-            mac:this.searchMAC,
-            resolving:this.searchResolving,
-            state:this.searchState,
-            schedule:this.searchSchedule,
-            system:this.searchSystem
+            group: this.searchGroup,
+            mac: this.searchMAC,
+            resolving: this.searchResolving,
+            state: this.searchState,
+            schedule: this.searchSchedule,
+            system: this.searchSystem,
           },
         })
         .then((res) => {
@@ -225,67 +297,70 @@ export default {
           this.tableData = res.data.records;
           this.total = res.data.total;
         });
+        request
+        .get("/device/schedule", {
+        })
+        .then((res) => {
+          console.log(res);
+          this.schedule = []
+          for (let i = 0; i < res.data.length; i++) {
+            var obj ={}
+            obj.value=res.data[i]
+            this.schedule.push(obj)
+          }
+        });
+        request
+        .get("/device/group", {
+        })
+        .then((res) => {
+          console.log(res);
+          this.group = []
+          for (let i = 0; i < res.data.length; i++) {
+            var obj ={}
+            obj.value=res.data[i]
+            this.group.push(obj)
+          }
+        });
     },
     reset() {
-      this.searchName='';
-      this.searchMechanism='';
-      this.searchGroup='';
-      this.searchMAC='';
-      this.searchResolving='';
-      this.searchState='';
-      this.searchSchedule='';
-      this.searchSystem='';
-    },
-    edit() {
-      this.dv1 = true;
-      this.form = {};
+      this.searchName = "";
+      this.searchMechanism = "";
+      this.searchGroup = "";
+      this.searchMAC = "";
+      this.searchResolving = "";
+      this.searchState = "";
+      this.searchSchedule = "";
+      this.searchSystem = "";
     },
     details() {
       this.dv2 = true;
       this.form = {};
     },
+    save1() {
+      this.dv2 = false; //关闭弹窗
+    },
     save() {
-      if (this.edit) {
-        request.put("/device", this.form).then((res) => {
-          console.log(res);
-          if (res.code === 200) {
-            this.$message({
-              type: "success",
-              message: "更新成功",
-            });
-          } else {
-            this.$message({
-              type: "error",
-              message: res.msg,
-            });
-          }
-          this.edit = 0;
-          this.load(); //刷新表格的数据
-          this.dv1 = false; //关闭弹窗
-        });
-      } else {
-        request.post("/device", this.form).then((res) => {
-          console.log(res);
-          if (res.code === 200) {
-            this.$message({
-              type: "success",
-              message: "新增成功",
-            });
-          } else {
-            this.$message({
-              type: "error",
-              message: res.msg,
-            });
-          }
-          this.load(); //刷新表格的数据
-          this.dv1 = false; //关闭弹窗
-        });
-      }
+      console.log(this.form);
+      request.put("/device", this.form).then((res) => {
+        console.log(res);
+        if (res.code === 200) {
+          this.$message({
+            type: "success",
+            message: "更新成功",
+          });
+        } else {
+          this.$message({
+            type: "error",
+            message: res.msg,
+          });
+        }
+        this.load(); //刷新表格的数据
+        this.dv1 = false; //关闭弹窗
+      });
     },
     handleEdit(row) {
       this.form = JSON.parse(JSON.stringify(row));
       this.dv1 = true;
-      this.edit = 1;
     },
     handleDelete(id) {
       console.log(id);
