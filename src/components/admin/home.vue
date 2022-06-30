b766fb852f08bef9b8b331c398e51e1dabaa139e
llyums <194773116@qq.com>
2022-06-30 10:44:55 +0800

    home

--- src/components/admin/home.vue
+++ src/components/admin/home.vue
@@ -132,6 +132,7 @@
                 placeholder="设备分布"
                 style="width: 30%"
                 clearable
+                @change="initChart()"
               >
                 <el-option
                   v-for="item in select"
@@ -152,17 +153,16 @@
         <el-card class="box-card2">
           <template #header>
             <div class="card-header">
-              <span>计划审核提醒</span>
+              <span>计划列表</span>
             </div>
           </template>
           <div class="card-body">
-            <el-table :data="tableData" style="width: 100%">
-              <el-table-column prop="date" label="序号" width="180" />
+            <el-table :data="tableData" max-height="200" style="width: 100%;">
+              <el-table-column fixed prop="id" label="序号" width="100" />
               <el-table-column prop="name" label="计划名称" width="180" />
-              <el-table-column prop="address" label="计划状态" />
-              <el-table-column prop="address" label="提交时间" />
-              <el-table-column prop="address" label="提交人" />
-              <el-table-column prop="address" label="操作" />
+              <el-table-column prop="state" label="计划状态" width="150"/>
+              <el-table-column prop="time" label="提交时间" width="230"/>
+              <el-table-column prop="person" label="提交人" />
             </el-table>
           </div>
         </el-card>
@@ -174,7 +174,22 @@
               <span>事件记录</span>
             </div>
           </template>
-          <div class="card-body"></div>
+          <div class="card-body">
+            <el-timeline>
+              <el-timeline-item
+                v-for="(activity, index) in activities"
+                :key="index"
+                :icon="activity.icon"
+                :type="activity.type"
+                :color="activity.color"
+                :size="activity.size"
+                :hollow="activity.hollow"
+                :timestamp="activity.timestamp"
+              >
+                {{ activity.content }}
+              </el-timeline-item>
+            </el-timeline>
+          </div>
         </el-card>
       </div>
     </div>
@@ -183,6 +198,7 @@
 <script>
 import request from "@/utils/request";
 import * as echarts from "echarts";
+import { MoreFilled } from "@element-plus/icons-vue";
 
 export default {
   data() {
@@ -202,68 +218,137 @@ export default {
           value: "机构",
         },
       ],
-      xData: ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"], //横坐标
-      yData: [2, 3, 1, 1, 2, 3, 4], //数据
+      xData: ["Mon", "Tue"], //横坐标
+      yData: [2, 3], //数据
       myChartStyle: { float: "left", width: "300%", height: "230px" }, //图表样式
 
       tableData: [
         {
-          date: "2016-05-03",
+          id: "2016-05-03",
           name: "Tom",
-          address: "No. 189, Grove St, Los Angeles",
+          state: "No. 189, Grove St, Los Angeles",
+          time: "2020/12/12",
+          person: "yyx",
+        }
+      ],
+      activities: [
+        {
+          content: "[计划]9868676计划被拒绝",
+          timestamp: "2022-06-28 10:50:22",
+          size: "large",
+          type: "primary",
+          icon: MoreFilled,
         },
         {
-          date: "2016-05-02",
-          name: "Tom",
-          address: "No. 189, Grove St, Los Angeles",
+          content: "[计划]12312312dad计划发布成功",
+          timestamp: "2022-06-27 15:50:46",
+          size: "large",
+          type: "primary",
+          icon: MoreFilled,
         },
         {
-          date: "2016-05-04",
-          name: "Tom",
-          address: "No. 189, Grove St, Los Angeles",
+          content: "[设备]测试误删设备已上线",
+          timestamp: "2022-06-27 15:50:24",
+          size: "large",
+          type: "primary",
+          icon: MoreFilled,
         },
         {
-          date: "2016-05-01",
-          name: "Tom",
-          address: "No. 189, Grove St, Los Angeles",
+          content: "[设备]测试误删设备注册成功",
+          timestamp: "2022-06-27 14:31:14",
+          size: "large",
+          type: "primary",
+          icon: MoreFilled,
         },
       ],
     };
   },
-  mounted() {
-    this.initEcharts();
-  },
+  mounted() {},
   created() {
     this.load();
   },
   methods: {
     load() {
       request.get("/device/sum", {}).then((res) => {
-        console.log(res);
+        //console.log(res);
         this.count1 = res.data;
       });
       request.get("/program/sum", {}).then((res) => {
-        console.log(res);
+        //console.log(res);
         this.count2 = res.data;
       });
       request.get("/schedule-list/sum", {}).then((res) => {
-        console.log(res);
+        //console.log(res);
         this.count3 = res.data;
       });
       request.get("/device/statesum", {}).then((res) => {
-        console.log(res);
+        //console.log(res);
         this.countLx = res.data[0];
         this.countKx = res.data[1];
         this.countBf = res.data[2];
       });
+      request.get("/grouping/group", {}).then((res) => {
+        // console.log(res);
+        //console.log(this.yData);
+        this.xData = [];
+        this.yData = [];
+        for (let i = 0; i < res.data.length; i++) {
+          this.xData.push(res.data[i].groupingName);
+          this.yData.push(res.data[i].groupingDevicecount);
+        }
+        //console.log(this.xData);
+        this.initEcharts();
+      });
+      request.get("/schedule-list/schedule", {}).then((res) => {
+        console.log(res);
+        this.tableData = [];
+        for (let i = 0; i < res.data.length; i++) {
+          var obj ={}
+          obj.id=res.data[i].listId;
+          obj.name=res.data[i].listName;
+          obj.state=res.data[i].listState;
+          obj.time=res.data[i].listUpdate;
+          obj.person=res.data[i].listAuthor;
+          this.tableData.push(obj);
+        }
+      });
+    },
+    initChart() {
+      if (this.selection == "分组") {
+        request.get("/grouping/group", {}).then((res) => {
+          //console.log(res);
+          this.xData = [];
+          this.yData = [];
+          for (let i = 0; i < res.data.length; i++) {
+            this.xData.push(res.data[i].groupingName);
+            this.yData.push(res.data[i].groupingDevicecount);
+          }
+          //console.log(this.xData);
+          this.initEcharts();
+        });
+      } else if (this.selection == "机构") {
+        request.get("/grouping/mechanism", {}).then((res) => {
+          //console.log(res);
+          this.xData = [];
+          this.yData = [];
+          for (let i = 0; i < res.data.length; i++) {
+            this.xData.push(res.data[i].groupingMechanism);
+            this.yData.push(res.data[i].groupingDevicecount);
+          }
+          //console.log(this.xData);
+          //console.log(this.yData);
+          this.initEcharts();
+        });
+      }
     },
     initEcharts() {
       // 基本柱状图
+
       const option = {
         xAxis: {
           data: this.xData,
         },
-        yAxis: {},
+        yAxis: { minInterval:1 },
         series: [
           {
             type: "bar", //形状为柱状图
@@ -385,12 +470,12 @@ export default {
   width: 540px;
 }
 .box-card2 {
-  height: 300px;
+  height: 320px;
   margin-left: 30px;
   width: 830px;
 }
 .box-card3 {
-  height: 300px;
+  height: 310px;
   margin-left: 30px;
   width: 540px;
 }
